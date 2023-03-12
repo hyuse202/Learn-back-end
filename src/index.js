eaf752cbd916cae1da4cb471bc57bb090f4c9e58
Your Name <you@example.com>
2023-03-12 22:30:22 +0700

    :truck: set up mongodb

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
-
 app.get('/', (req,res) => {
     return res.send('Hello world');
-});
+})
 
+mongoose.connect('mongodb+srv://hyuse202:huydepzai20022006@cluster0.gs3xbl9.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected mongodb')
+})
+.catch((err) => {
+    console.log(err);
+})
 app.listen(port, () => {
     console.log('Server is running...');
 });
